ff98f2e113451eef3471079eea6f3884897837ba
Jose Martines <zhittrion.exe01111000@gmail.com>
2026-03-09 22:22:58 -0400

    first commit

---
+++ calculadora.test.js
@@ -0,0 +1,15 @@
+It('Deve somar 1 + 1 e retornar 2', () => {
+    expect(1 + 1).toBe(2);
+});
+
+it('Deve subtrair 5 - 2 e retornar 3', () => {
+    expect(5 - 2).toBe(3);
+});
+
+it('Deve multiplicar 3 * 4 e retornar 12', () => {
+    expect(3 * 4).toBe(12);
+});
+
+it('Deve dividir 10 / 2 e retornar 5', () => {
+    expect(10 / 2).toBe(5);
+});
